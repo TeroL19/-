30b827c1eed819451da843d7337c29b4ae2fc357
TeroL19 <lokertero@gmail.com>
2025-05-28 21:57:40 +0300

    Добавлен основной файл Gems.cpp

---
+++ Gems.cpp
@@ -0,0 +1,14 @@
+﻿#include "GameHandler.hpp"
+
+int wmain(int argc, char* argv[]) {
+    GameModel model;
+
+    if (!model.getWindow() || !model.getRenderer()) {
+        return 1;
+    }
+
+    GameHandler game(model);
+    game.run();
+
+    return 0;
+}
